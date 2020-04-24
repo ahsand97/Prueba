d24923c0a8f30c99b8c0fc06366e3d1eeb7acf85
Luis David <luis_david_nino@hotmail.com>
2020-04-24 13:38:54 -0500

    Final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080'
-  apiUrl: 'https://protected-cliffs-43647.herokuapp.com/'
+  apiUrl: 'https://protected-cliffs-43647.herokuapp.com'
 };
 
 /*
